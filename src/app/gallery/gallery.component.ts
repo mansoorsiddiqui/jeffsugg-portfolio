13d15d59315c275077c8d622b88f0b6ca638241f
Mansoor Siddiqui <mis43@drexel.edu>
2020-01-28 12:35:16 -0500

    programmatic masonry grid v1

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
+  items = [];
+  width: number;
+  columns = 1;
 
-  constructor() { }
+  constructor(
+    private el: ElementRef,
+  ) { }
 
   ngOnInit() {
+    this.width = this.el.nativeElement.offsetWidth;
+    this.columns = (this.width > 1024) ? 3 : (this.width > 640) ? 2 : 1 ;
+    console.log(this.width, this.columns);
+    for (let i = 0; i < this.columns; i++) {
+      this.items.push([]);
+    }
+    for (let i = 1; i < 30; i++) {
+      this.items[(i-1)%this.columns].push({
+        title: i,
+        src: `./assets/${i}.jpg`,
+      })
+    }
+    console.log(this.items);
   }
 
 }

b35d1e5af37f4df6798514207223fc43cd1cc3a4
Mansoor Siddiqui <mis43@drexel.edu>
2020-01-29 17:52:01 -0500

    firebase function to create thumbnails, pulling thumbnails from firebase using web sdk, sorting masonry grid images evenly, tilt and hover effects on images

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,16 @@
 const functions = require('firebase-functions');
+const gc = require('@google-cloud/storage');
+const path = require('path');
+const sharp = require('sharp');
+const fs = require('fs-extra');
+const os = require('os');
+
+sharp.cache(false);
+const tmpdir = os.tmpdir;
+const join = path.join, dirname = path.dirname;
+
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +18,83 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+exports.createImageData = functions.storage.object().onFinalize(async (object) => {
+    const gcs = admin.storage();
+    const bucket = gcs.bucket(object.bucket);
+    const filePath = object.name;
+    const bucketDir = dirname(filePath);
+    const contentType = object.contentType;
+    if (!contentType.startsWith('image/')) {
+        console.log('Not an image');
+        return null;
+    }
+    const fileName = path.basename(filePath);
+    if (fileName.includes('1920w') || fileName.includes('640w')) {
+        console.log('Already a thumbnail');
+        return null;
+    }
+    const fileNameStripped = fileName.split('.').shift();
+    const workingDir = join(tmpdir(), 'thumbs');
+    const tmpFilePath = join(workingDir, 'source.jpg');
+
+    // 1. Ensure thumbnail dir exists
+    await fs.ensureDir(workingDir);
+
+    // 2. Download Source File
+    await bucket.file(filePath).download({
+      destination: tmpFilePath
+    });
+
+    // 3. Resize the images and define an array of upload promises
+    const sizes = [640, 1920];
+    let heights = {
+        640:null,
+        1920:null,
+    };
+
+    const uploadPromises = sizes.map(async size => {
+      const thumbName = `${fileNameStripped}_${size}w.jpg`;
+      const thumbPath = join(workingDir, thumbName);
+
+      // Resize source image
+      await sharp(tmpFilePath)
+        .resize(size, null)
+        .toFile(thumbPath)
+        .then((info) => heights[size] = info.height);
+
+      // Upload to GCS
+      return bucket.upload(thumbPath, {
+        destination: join(bucketDir, thumbName)
+      });
+    });
+
+    // 4. Run the upload operations
+    await Promise.all(uploadPromises);
+
+    // 5. Cleanup remove the tmp/thumbs from the filesystem
+    await fs.remove(workingDir)
+            .then(() => {
+                return fs.remove(tmpFilePath);
+            })
+            .then(() => {
+                return fs.remove(bucketDir);
+            });
+
+
+    let addImage = admin.firestore().collection('images').add({
+        name: fileName,
+        full: `https://firebasestorage.googleapis.com/v0/b/jeffsugg-portfolio.appspot.com/o/gallery%2F${fileNameStripped}_1920w.jpg?alt=media`,
+        thumb: `https://firebasestorage.googleapis.com/v0/b/jeffsugg-portfolio.appspot.com/o/gallery%2F${fileNameStripped}_640w.jpg?alt=media`,
+        fullHeight: heights[1920],
+        thumbHeight: heights[640],
+    }).then(ref => {
+        console.log('Added image with ID: ', ref.id);
+    });
+
+    return addImage.then(res => {
+        console.log('Add: ', res);
+    });
+
+});
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBY7sc5L1Snk3dFBBFA4BxrExg92mwl4KE",
+  authDomain: "jeffsugg-portfolio.firebaseapp.com",
+  databaseURL: "https://jeffsugg-portfolio.firebaseio.com",
+  projectId: "jeffsugg-portfolio",
+  storageBucket: "jeffsugg-portfolio.appspot.com",
+  messagingSenderId: "932264466430",
+  appId: "1:932264466430:web:ad5f5e70689b73ebb6d2af",
+  measurementId: "G-E1GKQTTZYP"
+};
+firebase.initializeApp(firebaseConfig);
 
 @Component({
   selector: 'app-gallery',
@@ -6,6 +20,7 @@ import { Component, OnInit, ElementRef } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
+  images = [];
   items = [];
   width: number;
   columns = 1;
@@ -14,20 +29,39 @@ export class GalleryComponent implements OnInit {
     private el: ElementRef,
   ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    const getImages = firebase.firestore().collection("images").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            this.images.push(doc.data());
+        });
+    });
+
+    await getImages;
+    this.images.sort(() => Math.random() - 0.5);
+
     this.width = this.el.nativeElement.offsetWidth;
     this.columns = (this.width > 1024) ? 3 : (this.width > 640) ? 2 : 1 ;
-    console.log(this.width, this.columns);
+
     for (let i = 0; i < this.columns; i++) {
       this.items.push([]);
     }
-    for (let i = 1; i < 30; i++) {
-      this.items[(i-1)%this.columns].push({
-        title: i,
-        src: `./assets/${i}.jpg`,
+
+    for (let i = 0; i < this.images.length; i++) {
+      this.items[this.getShortestColumn()].push({
+        title: this.images[i].name,
+        src: this.images[i].thumb,
+        height: this.images[i].thumbHeight,
       })
     }
+
     console.log(this.items);
+
+  }
+
+  getShortestColumn () {
+    const columnLengths = { ...this.items.map(col => col.reduce((a, b) => a+b.height, 0)) };
+    const shortest = Object.keys(columnLengths).sort((a,b)=> columnLengths[a]-columnLengths[b]).shift();
+    return shortest;
   }
 
 }
